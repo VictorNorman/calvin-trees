2802ff9155286dcf6621045f501679960b52c998
VictorNorman <vtn2@calvin.edu>
2023-05-02 12:35:15 -0400

    add pwa code; add 1 tree to db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private swUpdate: SwUpdate) {
+    this.checkForUpdates();
+  }
+
+  async checkForUpdates(): Promise<void> {
+
+    if (await this.swUpdate.checkForUpdate()) {
+      console.log('checkforUpdate returned true!');
+      if (confirm('A new version is available. Load it?')) {
+        try {
+          await this.swUpdate.activateUpdate();
+          confirm('New version activated!');
+          window.location.reload();
+        } catch {
+          console.log('FAILED to ACTIVATE new version!');
+        }
+      }
+    } else {
+      console.log('no new version found');
+    }
+
+
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { LngLat } from 'maplibre-gl';
+
+interface TreeInfo {
+  lng: number;
+  lat: number;
+  description: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+  public imageLoaded = false;
 
+  public nearbyTrees: TreeInfo[] = [];
+
+  private defaultLng = -85.5871801;
+  private defaultLat = 42.9308076;
+  public center: LngLat = new LngLat(this.defaultLng, this.defaultLat);
+  heading: [number] | undefined = undefined;
+  errorMsg: string = '';
+
+  private readonly treesDb: TreeInfo[] = [
+    {
+      lng: -85.59002609659022,
+      lat: 42.932341568753785,
+      description: "A happy tree!",
+    }
+  ];
+
+  constructor() {
+    window.navigator.geolocation.watchPosition(
+      (position) => {
+        if (position.coords.heading) {
+          this.heading = [position.coords.heading];
+        }
+        // update center of map.
+        this.center = new LngLat(position.coords.longitude, position.coords.latitude);
+        this.highlightNearbyTrees();
+      },
+      (error) => {
+        this.errorMsg = error.message;
+      },
+      {
+        enableHighAccuracy: true,
+      }
+    );
+
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  highlightNearbyTrees() {
+    this.nearbyTrees = this.treesDb.filter(tree =>
+      this.center.distanceTo(new LngLat(tree.lng, tree.lat)) < 10  // meters
+    );
+  }
 }
