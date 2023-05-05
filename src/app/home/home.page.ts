bd7e436864cbb80a3150347640bf97aea52318bb
VictorNorman <vtn2@calvin.edu>
2023-05-05 10:35:13 -0400

    basic wander mode is working! Got json file of tree information in assets/

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,10 +2,16 @@ import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MapComponent } from '@maplibre/ngx-maplibre-gl';
 import { LngLat } from 'maplibre-gl';
 
+
+import treeJson from '../../assets/trees.json';
+
 interface TreeInfo {
   lng: number;
   lat: number;
-  description: string;
+  commonName: string;
+  scientificName: string;
+  commemoration: string;
+  img?: string;
 }
 
 @Component({
@@ -28,16 +34,16 @@ export class HomePage implements AfterViewInit {
   @ViewChild('map') map: MapComponent | null = null;
 
   private readonly treesDb: TreeInfo[] = [
-    {
-      lng: -85.59002609659022,
-      lat: 42.932341568753785,
-      description: "Fancy pine",
-    },
-    {
-      lng: -85.591,
-      lat: 42.93245,
-      description: "Redwood",
-    }
+    // {
+    //   lng: -85.59002609659022,
+    //   lat: 42.932341568753785,
+    //   description: "Fancy pine",
+    // },
+    // {
+    //   lng: -85.591,
+    //   lat: 42.93245,
+    //   description: "Redwood",
+    // }
   ];
 
   constructor() {
@@ -58,6 +64,16 @@ export class HomePage implements AfterViewInit {
       }
     );
 
+    this.treesDb = treeJson.features.map((tree) => {
+      return {
+        lng: tree.geometry.coordinates[0],
+        lat: tree.geometry.coordinates[1],
+        scientificName: tree.properties.scientific,
+        commonName: tree.properties.common_nam,
+        commemoration: tree.properties.commemorat,
+      }
+    });
+
   }
 
   ngAfterViewInit() {
@@ -65,17 +81,9 @@ export class HomePage implements AfterViewInit {
   }
 
   highlightNearbyTrees() {
-    // setInterval(() => {
-    //   if (this.nearbyTrees) {
-    //     this.nearbyTrees = [];
-    //   } else {
-    this.nearbyTrees = this.treesDb;
-    //   }
-    //   console.log("hi");
-    // }, 2000);
-    // this.nearbyTrees = this.treesDb.filter(tree =>
-    //   this.center.distanceTo(new LngLat(tree.lng, tree.lat)) < 10  // meters
-    // );
+    this.nearbyTrees = this.treesDb.filter(tree =>
+      this.center.distanceTo(new LngLat(tree.lng, tree.lat)) < 10  // meters
+    );
   }
 }
 
@@ -88,5 +96,7 @@ last number is 1 - 113.
 Don't know how to get all trees...
 
 https://services2.arcgis.com/DBcRJmfPI2l07jMS/ArcGIS/rest/services/Calvin_Campus_Speelman_Arboretum_WFL1/FeatureServer/7/getEstimates/?f=pjson
+
+https://services2.arcgis.com/DBcRJmfPI2l07jMS/ArcGIS/rest/services/Calvin_Campus_Speelman_Arboretum_WFL1/FeatureServer/7/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=
 
 */
